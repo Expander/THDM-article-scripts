59d82e5bbd4193e675b38bca71d9c778950c1df7
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-08-27 12:19:54 +0200

    make mU3 and mQ3 a free parameter
    and do HSSUSY uncertainty scan for non-degenerate soft mass parameters

--- scan_HSSUSY_uncertainty.m
+++ scan_HSSUSY_uncertainty.m
@@ -20,7 +20,7 @@ index bca32d3d..fd127971 100644
 Mtpole = 173.34;
 AS = 0.1184;
 
-CalcDMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ] :=
+CalcDMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ, mQ3_?NumericQ, mU3_?NumericQ] :=
     CalcHSSUSYDMh[
         fsSettings -> {
             precisionGoal -> 1.*^-5,
@@ -40,8 +40,8 @@ CalcDMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ] :=
             MuInput -> MS,
             mAInput -> MS,
             AtInput -> (Xt + 1/TB) * MS,
-            msq2 -> MS^2 IdentityMatrix[3],
-            msu2 -> MS^2 IdentityMatrix[3],
+            msq2 -> DiagonalMatrix[{MS^2, MS^2, mQ3^2}],
+            msu2 -> DiagonalMatrix[{MS^2, MS^2, mU3^2}],
             msd2 -> MS^2 IdentityMatrix[3],
             msl2 -> MS^2 IdentityMatrix[3],
             mse2 -> MS^2 IdentityMatrix[3],
@@ -58,7 +58,7 @@ CalcDMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ] :=
         }
    ];
 
-CalcMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ, Mtp_:Mtpole, as_:AS] :=
+CalcMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ, mQ3_?NumericQ, mU3_?NumericQ, Mtp_:Mtpole, as_:AS] :=
     Module[{handle, spec},
            handle = FSHSSUSYOpenHandle[
                fsSettings -> {
@@ -80,8 +80,8 @@ CalcMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ, Mtp_:Mtpole, as_:
                    MuInput -> MS,
                    mAInput -> MS,
                    AtInput -> (Xt + 1/TB) * MS,
-                   msq2 -> MS^2 IdentityMatrix[3],
-                   msu2 -> MS^2 IdentityMatrix[3],
+                   msq2 -> DiagonalMatrix[{MS^2, MS^2, mQ3^2}],
+                   msu2 -> DiagonalMatrix[{MS^2, MS^2, mU3^2}],
                    msd2 -> MS^2 IdentityMatrix[3],
                    msl2 -> MS^2 IdentityMatrix[3],
                    mse2 -> MS^2 IdentityMatrix[3],
@@ -103,50 +103,84 @@ CalcMh[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ, Mtp_:Mtpole, as_:
               Pole[M[hh]] /. (HSSUSY /. spec)]
     ];
 
-CalcDMhParam[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ] :=
+CalcDMhParam[MS_?NumericQ, TB_?NumericQ, Xt_?NumericQ, MG_?NumericQ, mQ3_?NumericQ, mU3_?NumericQ] :=
     Module[{Mh, MhMtp, MhMtm, MhASp, MhASm},
-           Mh    = CalcMh[MS, TB, Xt, MG];
-           MhMtp = CalcMh[MS, TB, Xt, MG, Mtpole + 0.98];
-           MhMtm = CalcMh[MS, TB, Xt, MG, Mtpole - 0.98];
-           MhASp = CalcMh[MS, TB, Xt, MG, Mtpole, AS + 6 10^-4];
-           MhASm = CalcMh[MS, TB, Xt, MG, Mtpole, AS - 6 10^-4];
+           Mh    = CalcMh[MS, TB, Xt, MG, mQ3, mU3];
+           MhMtp = CalcMh[MS, TB, Xt, MG, mQ3, mU3, Mtpole + 0.98];
+           MhMtm = CalcMh[MS, TB, Xt, MG, mQ3, mU3, Mtpole - 0.98];
+           MhASp = CalcMh[MS, TB, Xt, MG, mQ3, mU3, Mtpole, AS + 6 10^-4];
+           MhASm = CalcMh[MS, TB, Xt, MG, mQ3, mU3, Mtpole, AS - 6 10^-4];
            (
                Abs[Max[{Mh, MhMtp, MhMtm}] - Min[{Mh, MhMtp, MhMtm}]] +
                Abs[Max[{Mh, MhASp, MhASm}] - Min[{Mh, MhASp, MhASm}]]
            ) / 2
           ];
 
-CalcDMhFixedTB[MS_, TB_, Xt_, MG_, Mh_] :=
+CalcDMhFixedTB[MS_, TB_, Xt_, MG_, mQ3_, mU3_, Mh_] :=
     Module[{xt, point},
-           point = FindRoot[CalcMh[MS,TB,xt,MG] == Mh, {xt, 1, 0, Xt},
+           point = FindRoot[CalcMh[MS,TB,xt,MG,mQ3,mU3] == Mh, {xt, 1, 0, Xt},
                             AccuracyGoal -> 4, PrecisionGoal -> 4];
            xt = If[point =!= {}, xt /. point, Xt];
            xt = If[NumericQ[xt], xt, Xt];
-           { MS, TB, xt, Sequence @@ CalcDMh[MS, TB, xt, MG], CalcDMhParam[MS, TB, xt, MG] }
+           { MS, TB, xt, Sequence @@ CalcDMh[MS, TB, xt, MG, mQ3, mU3], CalcDMhParam[MS, TB, xt, MG, mQ3, mU3] }
           ];
 
-CalcDMhFixedXt[MS_, TB_, Xt_, MG_, Mh_] :=
+CalcDMhFixedXt[MS_, TB_, Xt_, MG_, mQ3_, mU3_, Mh_] :=
     Module[{tb, point},
-           point = FindRoot[CalcMh[MS,tb,Xt,MG] == Mh, {tb, 2, 1, TB},
+           point = FindRoot[CalcMh[MS,tb,Xt,MG,mQ3,mU3] == Mh, {tb, 2, 1, TB},
                             AccuracyGoal -> 4, PrecisionGoal -> 4];
            tb = If[point =!= {}, tb /. point, TB];
            tb = If[NumericQ[tb], tb, TB];
-           { MS, tb, Xt, Sequence @@ CalcDMh[MS, tb, Xt, MG], CalcDMhParam[MS, tb, Xt, MG]}
+           { MS, tb, Xt, Sequence @@ CalcDMh[MS, tb, Xt, MG, mQ3, mU3], CalcDMhParam[MS, tb, Xt, MG, mQ3, mU3]}
           ];
 
 LaunchKernels[];
 DistributeDefinitions[CalcDMhFixedTB, CalcDMhFixedXt];
 
-dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[#], 125]&, LogRange[1000  , 2 10^4, 60]];
-dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[#], 125]&, LogRange[2 10^4, 10^10 , 60]];
+(* mU3 = mQ3 = MG = MS *)
+dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[#], N[#], N[#], 125]&, LogRange[1000  , 2 10^4, 60]];
+dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[#], N[#], N[#], 125]&, LogRange[1 10^4, 10^10 , 60]];
 
 Export["HSSUSY_uncertainty_plot_vary_Xt.dat", dataXt];
 Export["HSSUSY_uncertainty_plot_vary_TB.dat", dataTB];
 Export["HSSUSY_uncertainty_plot.dat", Join[dataXt, dataTB]];
 
-dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[2 #], 125]&, LogRange[1000  , 2 10^4, 60]];
-dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[2 #], 125]&, LogRange[2 10^4, 10^10 , 60]];
+(* mU3 = mQ3 = MG/2 = MS *)
+dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[2 #], N[#], N[#], 125]&, LogRange[1000  , 2 10^4, 60]];
+dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[2 #], N[#], N[#], 125]&, LogRange[1 10^4, 10^10 , 60]];
 
 Export["HSSUSY_uncertainty_plot_vary_Xt_MG-2MS.dat", dataXt];
 Export["HSSUSY_uncertainty_plot_vary_TB_MG-2MS.dat", dataTB];
 Export["HSSUSY_uncertainty_plot_MG-2MS.dat", Join[dataXt, dataTB]];
+
+(* mU3 = mQ3/1.5 = MG = MS *)
+dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[#], N[1.5 #], N[#], 125]&, LogRange[1000  , 2 10^4, 60]];
+dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[#], N[1.5 #], N[#], 125]&, LogRange[1 10^4, 10^10 , 60]];
+
+Export["HSSUSY_uncertainty_plot_vary_Xt_mQ3-1.5MS.dat", dataXt];
+Export["HSSUSY_uncertainty_plot_vary_TB_mQ3-1.5MS.dat", dataTB];
+Export["HSSUSY_uncertainty_plot_mQ3-1.5MS.dat", Join[dataXt, dataTB]];
+
+(* mU3/1.5 = mQ3 = MG = MS *)
+dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[#], N[#], N[1.5 #], 125]&, LogRange[1000  , 2 10^4, 60]];
+dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[#], N[#], N[1.5 #], 125]&, LogRange[1 10^4, 10^10 , 60]];
+
+Export["HSSUSY_uncertainty_plot_vary_Xt_mU3-1.5MS.dat", dataXt];
+Export["HSSUSY_uncertainty_plot_vary_TB_mU3-1.5MS.dat", dataTB];
+Export["HSSUSY_uncertainty_plot_mU3-1.5MS.dat", Join[dataXt, dataTB]];
+
+(* mU3/0.8 = mQ3/1.2 = MG = MS *)
+dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[#], N[1.2 #], N[0.8 #], 125]&, LogRange[1000  , 2 10^4, 60]];
+dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[#], N[1.2 #], N[0.8 #], 125]&, LogRange[1 10^4, 10^10 , 60]];
+
+Export["HSSUSY_uncertainty_plot_vary_Xt_mQ3-1.2MS_mU3-0.8MS.dat", dataXt];
+Export["HSSUSY_uncertainty_plot_vary_TB_mQ3-1.2MS_mU3-0.8MS.dat", dataTB];
+Export["HSSUSY_uncertainty_plot_mQ3-1.2MS_mU3-0.8MS.dat", Join[dataXt, dataTB]];
+
+(* mU3/0.8 = mQ3/1.2 = MG/1.8 = MS *)
+dataXt = ParallelMap[CalcDMhFixedTB[N[#], 20, N[Sqrt[6]], N[1.8 #], N[1.2 #], N[0.8 #], 125]&, LogRange[1000  , 2 10^4, 60]];
+dataTB = ParallelMap[CalcDMhFixedXt[N[#], 20,          0, N[1.8 #], N[1.2 #], N[0.8 #], 125]&, LogRange[1 10^4, 10^10 , 60]];
+
+Export["HSSUSY_uncertainty_plot_vary_Xt_MG-1.8MS_mQ3-1.2MS_mU3-0.8MS.dat", dataXt];
+Export["HSSUSY_uncertainty_plot_vary_TB_MG-1.8MS_mQ3-1.2MS_mU3-0.8MS.dat", dataTB];
+Export["HSSUSY_uncertainty_plot_MG-1.8MS_mQ3-1.2MS_mU3-0.8MS.dat", Join[dataXt, dataTB]];
